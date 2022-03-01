3a7c0e64a2491ac115a4b84606c3a0cc7d925baf
“PrinceXxShadows” <princexxshadows@gmail.com>
2022-03-01 09:17:59 -0600

    excercise complete

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log(17);
+/*
+Strings
+Booleans
+Null
+Undefined
+Numbers
+*/
+
+// Addition is +
+// Subtraction is -
+// Multiplication is *
+// Division is /
+// Modulus is %
+// Exponentiation is **
+
+console.log(5 % 4);
+console.log(5 % 2);
+
+console.log("'I'M MARY POPPINS, Y'ALL'");
+
+console.log( "Hawk" + "eye");
+
+console.log("Paul".length + "Rogers".length);
+
+console.log( "Avengers: Endgame".length);
+
+
+console.log("\\");
+
+console.log((5 ** 4)-9);
+
+console.log('I'.length + "Am".length % "Iron".length - "Man".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THORN"[3]);
+console.log("STAR-LORD"[0]);
+
+ let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
